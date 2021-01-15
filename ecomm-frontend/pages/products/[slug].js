b070452865d8674d133dcc12e90c82585e3ca68f
Art I <sovietbulldozer@gmail.com>
2021-01-15 16:07:08 -0500

    Recieve proper API response and display to NextJS context

--- ecomm-frontend/pages/products/[slug].js
+++ ecomm-frontend/pages/products/[slug].js
@@ -1,10 +1,8 @@
 import Head from 'next/head';
-import products from '../../products.json';
-import { fromImageToUrl } from '../../utils/urls';
+import { fromImageToUrl, API_URL } from '../../utils/urls';
 import { twoDecimals } from '../../utils/format';
-const product = products[0];
 
-const Product = () => {
+const Product = ({ product }) => {
   return (
     <div>
       {/* using next.js head to create seo */}
@@ -22,5 +20,29 @@ const Product = () => {
     </div>
   );
 };
+
+export async function getStaticProps({ params: { slug } }) {
+  const product_res = await fetch(`${API_URL}/products/?slug=${slug}`);
+  const found = await product_res.json();
+  return {
+    props: {
+      product: found[0], //because the API response for filters is an array
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  //retreive all possible paths
+  const products_res = await fetch(`${API_URL}/products/`);
+  const products = await products_res.json();
+
+  //return them to nextjs context
+  return {
+    paths: products.map((product) => ({
+      params: { slug: String(product.slug) },
+    })),
+    fallback: false, //tells nextjs to show a 404 if param is not a match
+  };
+}
 
 export default Product;
